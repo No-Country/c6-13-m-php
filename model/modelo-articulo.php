7b2b416c06e8b7368c6bb9e2d668f5b46cc3cf5e
rony182 <rony.182@hotmail.com>
2022-08-25 14:19:44 -0300

    Agregando controlador y views para los artículos

--- model/modelo-articulo.php
+++ model/modelo-articulo.php
@@ -5,7 +5,7 @@
 // agregarArtículo() function will handle adding new articules to the inventory
 function agregarArticulo($nombre_articulo, $cod_categoria, $unidad_medida, $cantidad_articulo, $fecha_vencimiento, $cod_usuario, $estado, $fecha_ingreso){
   //create a connection object using the connection to the database
-  $db = conexion();
+  $db = conectar();
   //The SQL statement
   $sql = 'INSERT INTO tbl_articulos (nombre_articulo, cod_categoria, unidad_medida, cantidad_articulo, fecha_vencimiento, cod_usuario, estado, fecha_ingreso)
   VALUES (:nombre_articulo, :cod_categoria, :unidad_medida, :cantidad_articulo, :fecha_vencimiento, :cod_usuario, :estado, :fecha_ingreso)';
@@ -35,9 +35,9 @@ function agregarArticulo($nombre_articulo, $cod_categoria, $unidad_medida, $cant
 
 function obtenerInfoArticulo($id_articulo){
   //create a connection object using the connection to the database
-  $db = conexion();
+  $db = conectar();
   //The SQL statement
-  $sql = 'SELECT * FROM tbl_acticulos WHERE id_articulo = :id_articulo';
+  $sql = 'SELECT * FROM tbl_articulos WHERE id_articulo = :id_articulo';
   //Create the prepared statement using the database connection
   $stmt = $db->prepare($sql);
   //The next line replace the placeholder in the SQL statement
@@ -52,4 +52,46 @@ function obtenerInfoArticulo($id_articulo){
   $stmt->closeCursor();
   //Return the indication of succes (rows changed)
   return $info_articulo;
+}
+
+function editarArticulo($nombre_articulo, $cod_categoria, $unidad_medida, $cantidad_articulo, $fecha_vencimiento, $estado, $id_articulo){
+  //create a connection object using the php motors connection
+  $db = conectar();
+  //the SQL statement
+  $sql = 'UPDATE tbl_articulos SET nombre_articulo = :nombre_articulo, cod_categoria = :cod_categoria, unidad_medida = :unidad_medida,
+  cantidad_articulo = :cantidad_articulo, fecha_vencimiento = :fecha_vencimiento, estado = :estado
+  WHERE id_articulo = :id_articulo';
+  //create the prepared statement using the php motors connection
+  $stmt = $db->prepare($sql);
+  //The next nine lines replace the placeholders in the SQL
+  //statement with the actual values in the variables
+  //and tells the database the type of data it is 
+  $stmt->bindValue(':nombre_articulo', $nombre_articulo, PDO::PARAM_STR);
+  $stmt->bindValue(':cod_categoria', $cod_categoria, PDO::PARAM_INT);
+  $stmt->bindValue(':unidad_medida', $unidad_medida, PDO::PARAM_STR);
+  $stmt->bindValue(':cantidad_articulo', $cantidad_articulo, PDO::PARAM_INT);
+  $stmt->bindValue(':fecha_vencimiento', $fecha_vencimiento, PDO::PARAM_STR);
+  $stmt->bindValue(':estado', $estado, PDO::PARAM_STR);
+  $stmt->bindValue(':id_articulo', $id_articulo, PDO::PARAM_INT);
+  //insert the data
+  $stmt->execute();
+  //ask how maany rows changed as a result of our insert
+  $rowsChanged = $stmt->rowCount();
+  //close the database interaction
+  $stmt->closeCursor();
+  //return the indication of succes (rows changed)
+  return $rowsChanged;
+}
+
+function obtenerArticuloPorUsuario($cod_usuario){
+  $db = conectar();
+  $sql = 'SELECT *
+  FROM tbl_articulos
+  WHERE cod_usuario = :cod_usuario';
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue(':cod_usuario', $cod_usuario, PDO::PARAM_INT);
+  $stmt->execute();
+  $articulos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $stmt->closeCursor();
+  return $articulos;
 }
