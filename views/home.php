d504f4b9a79170d28282fd8da931e7b8c1c82b73
rony182 <rony.182@hotmail.com>
2022-08-30 13:05:56 -0300

    adding and editing html and correcting errors

--- views/home.php
+++ views/home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stock-AR</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/small.css"'>
+    <link rel="stylesheet" href="./css/small.css">
     <link rel="stylesheet" href='./css/medium.css'>
     <link rel="stylesheet" href='./css/large.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
@@ -27,7 +27,7 @@
                 <div  class="img-seccion" id="img-crear-lista">
                   
                 </div>
-               <a href="/c6-13-m-php/articulos"><button class="btn btn-primary mt-3">Agregar productos al Stock</button></a> 
+               <a class="btn btn-primary mt-3" href="/c6-13-m-php/articulos">Agregar productos al Stock</a>
             </div>
             <div class="col-md-4 text-center">
               <h1 class="titulo-funciones">Ultima Lista</h1>
@@ -53,7 +53,4 @@
             </footer>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 </html>
